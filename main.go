ab13566880b73afce95a78459edc83ab86ba496e
椒盐送来 <liqingitt@gmail.com>
2026-09-19 22:37:56 +0800

    feat: 测试一次提交

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"doc-precise-rag/knowledge/repository"
 	"doc-precise-rag/knowledge/router"
 	"doc-precise-rag/knowledge/service"
+	"fmt"
 )
 
 func main() {
@@ -14,9 +15,9 @@ func main() {
 	defer clients.DB.Close()
 	commonSvc := service.NewCommonService(clients.CosClient)
 	commonHandler := handler.NewCommonHandler(commonSvc)
-
 	importGraphCompiledInstance := processimport.ImportGraphCompiledInstance
 
+	fmt.Println("测试 commit  ")
 	docOriginFileRepository := repository.NewDocOriginFileRepository(clients.DB, clients.SnowflakeClient)
 	docOriginFileService := service.NewDocOriginFileService(docOriginFileRepository, importGraphCompiledInstance)
 	docOriginFileHandler := handler.NewDocHandler(docOriginFileService)
